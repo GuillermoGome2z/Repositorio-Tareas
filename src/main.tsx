57a21dc3c9a52a89c66f0821326c4b60139022cd
GuillermoGome2z <gomezguille391@gmail.com>
2025-08-10 17:14:39 -0600

    Editamos los archivos para visiaulizarlos en la pagina

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+
+const router = createBrowserRouter([{ path: '*', element: <App /> }]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
